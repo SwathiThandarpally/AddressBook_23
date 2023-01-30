54c9ba6896e7789867d880857e0c7ca0a5df9225
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-30 21:55:33 +0530

    Added Delete Contact Program

--- AddressBook_23/Program.cs
+++ AddressBook_23/Program.cs
@@ -9,7 +9,7 @@ namespace AddressBook_23
         {
             Console.WriteLine("Displaying Address Book Program");
             //UC_1
-            CreateContact create = new CreateContact();
+            //CreateContact create = new CreateContact();
             //MainAddress address = new MainAddress();
             //address.CreateContact();
             ////UC_2
@@ -17,12 +17,16 @@ namespace AddressBook_23
             //MainAddress mainAddress = new MainAddress();
             //add.CreateContact();
             //add.Display();
-            //UC_3
-            EditContact edit = new EditContact();
-            edit.CreateContact();
-            edit.Display();
-            edit.Edit();
-
+            ////UC_3
+            //EditContact edit = new EditContact();
+            //edit.CreateContact();
+            //edit.Display();
+            //edit.Edit();
+            //UC_4
+            DeleteContact delete = new DeleteContact();
+           // delete.CreateContact();
+            //delete.Display();
+            delete.Delete();
         }
     }
 }

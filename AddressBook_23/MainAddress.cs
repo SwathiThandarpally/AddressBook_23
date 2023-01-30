93be6d731a31892ef7fde654e08ef9d80ea329f1
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-30 19:49:57 +0530

    Added CreatContact Program

---
+++ AddressBook_23/MainAddress.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBook_23
+{
+    public  class MainAddress
+    {
+        CreateContact contact = new CreateContact();
+        public void CreateContact()
+        {
+            Console.WriteLine("Enter First Name : ");
+            contact.FirstName = Console.ReadLine();
+            Console.WriteLine("Enter Last Name : ");
+            contact.LastName = Console.ReadLine();
+            Console.WriteLine("Enter Address : ");
+            contact.Address = Console.ReadLine();
+            Console.WriteLine("Enter City : ");
+            contact.City = Console.ReadLine();
+            Console.WriteLine("Enter State : ");
+            contact.State = Console.ReadLine();
+            Console.WriteLine("Enter Zip Code : ");
+            contact.Zip = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Phone Number : ");
+            contact.PhoneNumber = (int)Convert.ToInt64(Console.ReadLine());
+            Console.WriteLine("Enter Email Address : ");
+            contact.Email = Console.ReadLine();
+        }
+    }
+}
--- AddressBook_23/Program.cs
+++ AddressBook_23/Program.cs
@@ -1,10 +1,18 @@
-﻿namespace AddressBook_23
+﻿using System;
+using AddressBook_23;
+
+namespace AddressBook_23
 {
     class Program
     {
         public static void Main(string[] args)
         {
             Console.WriteLine("Displaying Address Book Program");
+            //UC_1
+            CreateContact create = new CreateContact();
+            MainAddress address = new MainAddress();
+            address.CreateContact();
+
         }
     }
 }

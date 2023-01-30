0b41d068b9c1a9d4946000aa340daed25f1b457a
SwathiThandarpally <114502982+SwathiThandarpally@users.noreply.github.com>
2023-01-30 22:11:54 +0530

    Added Multiple Contacts Program

--- AddressBook_23/Program.cs
+++ AddressBook_23/Program.cs
@@ -9,24 +9,29 @@ namespace AddressBook_23
         {
             Console.WriteLine("Displaying Address Book Program");
             //UC_1
-            //CreateContact create = new CreateContact();
-            //MainAddress address = new MainAddress();
-            //address.CreateContact();
-            ////UC_2
-            //AddContact add = new AddContact();
-            //MainAddress mainAddress = new MainAddress();
-            //add.CreateContact();
-            //add.Display();
-            ////UC_3
-            //EditContact edit = new EditContact();
-            //edit.CreateContact();
-            //edit.Display();
-            //edit.Edit();
+            CreateContact create = new CreateContact();
+            MainAddress address = new MainAddress();
+            address.CreateContact();
+            //UC_2
+            AddContact add = new AddContact();
+            MainAddress mainAddress = new MainAddress();
+            add.CreateContact();
+            add.Display();
+            //UC_3
+            EditContact edit = new EditContact();
+            edit.CreateContact();
+            edit.Display();
+            edit.Edit();
             //UC_4
             DeleteContact delete = new DeleteContact();
-           // delete.CreateContact();
-            //delete.Display();
+            delete.CreateContact();
+            delete.Display();
             delete.Delete();
+            //UC_5
+            MultipleContacts contacts = new MultipleContacts();
+            contacts.AddContact();
+            contacts.ViewContact();
+            contacts.Display();
         }
     }
 }
